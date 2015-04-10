6a8a34cbea85d74c0f7cd16b297df21293a44f2f
Bryn Stringer <BStringer@BSTRINGER-PC8.ie.com>
2015-04-10 16:03:08 +0100

    Start of my cvsite
    Github and linkedin embedded, basic flow.

---
+++ BrynCVSite/Controllers/PlayController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BrynCVSite.Controllers
+{
+    public class PlayController : Controller
+    {
+        //
+        // GET: /Play/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ BrynCVSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BrynCVSite.MvcApplication" Language="C#" %>
